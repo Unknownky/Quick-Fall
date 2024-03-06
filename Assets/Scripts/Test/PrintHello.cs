f44be01162d2734e8616c74bc664f06a79086815
BuZhiDao <3085017810@qq.com>
2024-03-06 20:18:58 +0800

    测试在prefab上挂载脚本
    会显示The reference script on this Behaviour is missing，prefabs上的脚本只是一些引用的数字

---
+++ Assets/Scripts/Test/PrintHello.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintHello : MonoBehaviour
+{
+    private void Start() {
+        Debug.Log("Hello World!");
+    }
+}
